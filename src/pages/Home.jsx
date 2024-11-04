8ce9c4ba8c5c7964411ae331725e399644188079
Bennet Bechtel <bennet.bechtel007@gmail.com>
2024-11-04 21:52:25 +0100

    UI Buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import HeroElement from "../components/HeroElement";
+import Button from "../components/Button";
 
 const Home = () => {
   return (
-    <div className="flex flex-col px-3">
+    <div className="flex flex-col gap-5 px-3">
       <HeroElement />
+      <Button
+        text="Primary Button"
+        onClick={() => console.log("Click!")}
+        size="sm"
+        type="primary"
+      />
+      <Button
+        text="Secondary Button"
+        onClick={() => console.log("Click!")}
+        size="sm"
+        type="secondary"
+      />
+      <Button
+        text="Outlined Button"
+        onClick={() => console.log("Click!")}
+        size="sm"
+        type="outlined"
+      />
+      <Button
+        text="Elevated Button"
+        onClick={() => console.log("Click!")}
+        size="sm"
+        type="elevated"
+      />
     </div>
   );
 };
