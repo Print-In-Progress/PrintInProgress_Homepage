aa40f03be2ba7215c649f27c62caded3ae6e7826
Bennet Bechtel <bennet.bechtel007@gmail.com>
2024-11-05 18:32:08 +0100

    HeroElement fix

--- src/components/HeroElement.jsx
+++ src/components/HeroElement.jsx
@@ -3,22 +3,18 @@ import HeroImage from "../assets/HeroImage.png";
 
 const HeroElement = () => {
   return (
-    <div className="relative mx-auto flex w-full max-w-7xl items-start justify-center px-5">
-      <img
-        src={HeroImage}
-        alt="Product"
-        className="relative inset-0 h-full max-h-screen w-full max-w-7xl object-cover brightness-75"
-      />
-      <section className="absolute left-0 top-0 z-10 flex w-fit flex-col text-7xl font-bold text-white">
+    <div className="relative mx-auto flex w-full max-w-screen-2xl items-start justify-center px-5">
+      <section className="absolute left-0 top-0 z-10 flex w-fit flex-col text-6xl font-bold text-white sm:text-7xl">
         <span>Connect</span>
         <span>your</span>
         <ReactRotatingText
           items={["School", "Organization", "University"]}
-          pause={1500}
+          pause={2000}
           typingInterval={100}
           deletingInterval={100}
         />
       </section>
+      <img src={HeroImage} alt="Product" className="max-h-screen" />
     </div>
   );
 };
