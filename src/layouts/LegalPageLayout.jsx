614134d32b773f2795c0cde3529d74b2461ccacf
Bennet Bechtel <bennet.bechtel007@gmail.com>
2024-11-11 23:09:24 +0100

    LegalLayout heigth fix

--- src/layouts/LegalPageLayout.jsx
+++ src/layouts/LegalPageLayout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LegalPageLayout = ({ title, lastUpdated, children }) => {
   return (
-    <div className="mb-5 mt-5 flex min-h-screen flex-col px-3 py-2">
+    <div className="mb-5 mt-5 flex flex-col px-3 py-2">
       <div className="mx-auto w-full max-w-4xl">
         <h1 className="mb-6 text-display-md text-gray-display">{title}</h1>
         <p className="mb-8 text-body text-gray-body">
