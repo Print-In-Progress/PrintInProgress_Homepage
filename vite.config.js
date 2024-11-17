ae0809d3093d57bd166bb406e5ff8b995fca6c14
Bennet Bechtel <bennet.bechtel007@gmail.com>
2024-11-17 22:32:54 +0100

    Build fix

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,15 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
+    target: "esnext", // This enables support for top-level await
     commonjsOptions: {
-      include: [/\.json$/],
+      include: [/node_modules/],
     },
   },
   optimizeDeps: {
-    include: ["*.json"],
-  },
-  // Enable JSON imports
-  json: {
-    stringify: true,
+    include: ["react", "react-dom", "react-router-dom"],
   },
 });
