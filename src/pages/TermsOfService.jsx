caac1fd0d97dff7162bb17ab001d4966b2da42a2
Bennet Bechtel <bennet.bechtel007@gmail.com>
2024-11-23 01:13:49 +0100

    Terms Of Service Real TOS

--- src/pages/TermsOfService.jsx
+++ src/pages/TermsOfService.jsx
@@ -10,21 +10,53 @@ const TermsOfService = () => {
       title="Terms of Service"
       lastUpdated={termsConfig.lastUpdated}
     >
-      {termsConfig.sections.map((section) => (
-        <section key={section.id}>
-          <h2 className="mb-4 text-headline text-gray-display">
-            {section.title}
+      {
+        <section className="flex flex-col gap-3">
+          <h2 className="text-display-sm text-gray-headline">
+            {termsConfig.introduction.title}
           </h2>
-          <p className="text-body text-gray-body">{section.content}</p>
-          {section.bulletPoints && (
-            <ul className="mt-4 list-disc pl-6 text-body text-gray-body">
-              {section.bulletPoints.map((point, index) => (
-                <li key={index} className="mb-2">
-                  {point}
-                </li>
+          {termsConfig.introduction.content.map((part, index) => (
+            <p key={index} className="text-body text-gray-body">
+              {part}
+            </p>
+          ))}
+        </section>
+      }
+      {termsConfig.mainPart.map((part, index) => (
+        <section key={part.id} className="flex flex-col gap-3">
+          <h2 className="text-display-sm text-gray-headline">
+            {index + 1}. {part.title}
+          </h2>
+
+          {part.sections.map((section) => (
+            <span key={section.id} className="flex flex-col gap-2">
+              <h3 className="text-headline text-gray-headline"></h3>
+
+              {section.content.map((cont, index) => (
+                <span key={section.id + index}>
+                  {cont.type == "normal" && (
+                    <p className="text-body text-gray-body">{cont.text}</p>
+                  )}
+
+                  {cont.type == "bullet" && (
+                    <ul className="flex list-disc flex-col pl-6 text-body text-gray-body">
+                      {cont.points.map((point, index) => (
+                        <li key={index + section.id}>{point}</li>
+                      ))}
+                    </ul>
+                  )}
+
+                  {cont.type == "contact" && (
+                    <span className="flex list-disc flex-col text-body font-bold text-gray-display">
+                      {cont.info.map((point, index) => (
+                        <p key={index + section.id}>{point}</p>
+                      ))}
+                    </span>
+                  )}
+                </span>
               ))}
-            </ul>
-          )}
+            </span>
+          ))}
         </section>
       ))}
     </LegalPageLayout>
